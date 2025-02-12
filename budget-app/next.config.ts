d10e9b87e14c889462eb191c921972f7f32a88b7
Guillaume EGU <guillaume.egu@gmail.com>
2025-02-12 11:40:30 +0100

    CFV2

--- budget-app/next.config.ts
+++ budget-app/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+/** 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here 
 };
 
 export default nextConfig;
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // ✅ Obligatoire pour Next.js 13+
+  },
+};
+
+module.exports = nextConfig;
