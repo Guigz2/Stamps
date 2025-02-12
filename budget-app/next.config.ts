3ef37ac0167606c0c120ef8849b0befd04707e89
Guillaume EGU <guillaume.egu@gmail.com>
2025-02-12 11:53:26 +0100

    NA

--- budget-app/next.config.ts
+++ budget-app/next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-/** 
-const nextConfig: NextConfig = {
-  /* config options here 
-};
-
-export default nextConfig;
-*/
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
